87467d5a705b02d1e80228db467f5f612e120e4d
Prashant <40064519+Prashant2204@users.noreply.github.com>
2019-10-01 01:36:48 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+
+import os
+
+from flask import Flask, render_template, redirect, request, url_for
+
+from flask_pymongo import PyMongo
+
+from bson.objectid import ObjectId 
+
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'update_keys'
+
+app.config["MONGO_URI"] = 'mongodb+srv://bhatia1097:pd1097@mycluster-qykbg.mongodb.net/update_keys?retryWrites=true&w=majority'
+
+
+
+mongo = PyMongo(app)
+
+
+
+
+
+@app.route('/')
+
+@app.route('/get_keys')
+
+def get_keys():
+
+    return render_template("keys.html", keys=mongo.db.keys.find())
+
+
+@app.route('/add_key')
+
+def add_key():
+
+    return render_template('addkey.html',
+
+                           categories=mongo.db.categories.find())
+
+@app.route('/insert_key', methods=['POST'])
+
+def insert_key():
+
+    keys = mongo.db.keys
+
+    keys.insert_one(request.form.to_dict())
+
+    return redirect(url_for('get_keys'))
+
+
+if __name__ == '__main__':
+
+    app.run(host=os.environ.get('IP'),
+
+            port=(os.environ.get('PORT')),
+
+            debug=True)
